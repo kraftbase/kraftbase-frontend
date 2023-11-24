14220782041282e6c9f81bc451ae3e71946b5ee2
Shantanu Vispute <shantanuvispute02@gmail.com>
2023-11-24 18:15:46 +0530

    add:updated mobile layout (services section)

--- src/app/components/service/services.tsx
+++ src/app/components/service/services.tsx
@@ -15,7 +15,7 @@ export default function Services({}: Props) {
           tincidunt lectus, sed iaculis mauris varius at.
         </div>
       </div>
-      <div className="mx-auto mt-14 flex w-[85%] flex-col gap-10 self-start font-righteous text-[30px] uppercase max-sm:w-full">
+      <div className="mx-auto mt-14 flex w-[85%] flex-col gap-10 self-start font-righteous text-[30px] uppercase max-sm:hidden">
         <div className="text-slate-500">Application Development</div>
 
         <div className="text-slate-500">Development</div>
@@ -27,6 +27,39 @@ export default function Services({}: Props) {
         <div className="text-slate-500">Product Design</div>
 
         <div className="text-slate-500">Website Development</div>
+      </div>
+      <div className="mx-auto mt-14 hidden items-center font-righteous text-[30px] font-normal uppercase max-sm:block max-sm:w-full">
+        <div className="flex flex-col items-center gap-4 text-center">
+          <div className="text-black dark:text-white">Strategy</div>
+          <div className="text-[18px] capitalize text-slate-500">
+            <div>Strategy</div>
+            <div>Discovery & Research</div>
+            <div>User Experience</div>
+            <div>Content Strategy</div>
+          </div>
+        </div>
+        <div className="mt-16 flex flex-col items-center gap-4 text-center">
+          <div className="text-black dark:text-white">Design</div>
+          <div className="text-[18px] capitalize text-slate-500">
+            <div>Brand Development & Rebranding</div>
+            <div>Logo & ID Systems</div>
+            <div>Brand Style Guides</div>
+            <div>Collateral, Print & Packaging</div>
+            <div>User Interface</div>
+            <div>Website Design</div>
+            <div>UX/UI</div>
+          </div>
+        </div>
+        <div className="mt-16 flex flex-col items-center gap-4 text-center">
+          <div className="text-black dark:text-white">Development</div>
+          <div className="text-[18px] capitalize text-slate-500">
+            <div>Website Design & Development</div>
+            <div>Web Applications</div>
+            <div>Android Applications</div>
+            <div>Blockchain Development</div>
+            <div>Ecommerce</div>
+          </div>
+        </div>
       </div>
     </div>
   );
